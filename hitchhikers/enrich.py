1658f150d69efd88fe57440e48f6d83cd296422d
jaypark81 <jaypark81@gmail.com>
2026-05-03 16:13:25 +0200

    fix: revoke userInfo key in enrich.py

--- hitchhikers/enrich.py
+++ hitchhikers/enrich.py
@@ -28,7 +28,7 @@ def enrich(response: dict):
                 logger.error("[SKIP] cannot resolve uid")
                 return
         namespace = req.get('namespace', '')
-        username = req['request']['userInfo']['username']
+        username = req['userInfo']['username']
         groups = ','.join(req['userInfo'].get('groups', []))
         fieldManager = req.get('options', {}).get('fieldManager', '')
 
