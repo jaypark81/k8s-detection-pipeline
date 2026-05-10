e9c5347f1fca9e5acb54319234c6a2eb5b1d5070
jaypark81 <jaypark81@gmail.com>
2026-05-10 18:08:32 +0200

    refactor: normalize Hitchhiker metadata to nested JSON structure

--- hitchhikers/models.py
+++ hitchhikers/models.py
@@ -15,48 +15,48 @@ SENSITIVE_HOST_PATHS = {
 
 @dataclass
 class ContainerMetadata:
-    name: str = field(default=None, metadata={'key': 'container.name'})
-    image: str = field(default=None, metadata={'key': 'container.image'})
-    listeningPorts: str = field(default=None, metadata={'key': 'container.ports'})
-    env: str = field(default=None, metadata={'key': 'container.env'})
-    privileged: str = field(default=None, metadata={'key': 'container.privileged'})
-    capabilities_added: str = field(default=None, metadata={'key': 'container.capabilities.added'})
-    capabilities_dropped: str = field(default=None, metadata={'key': 'container.capabilities.dropped'})
-    allowPrivilegeEscalation: str = field(default=None, metadata={'key': 'container.allowprivilegeescalation'})
-    runAsUser: str = field(default=None, metadata={'key': 'container.runAsUsers'})
-    runAsNonRoot: str = field(default=None, metadata={'key': 'container.runAsNonRoot'})
-    listeningPorts: str = field(default=None, metadata={'key': 'container.listeningPorts'})
-    command: str = field(default=None, metadata={'key': 'container.command'})
-    args: str = field(default=None, metadata={'key': 'container.args'})
+    name: str = field(default=None)
+    image: str = field(default=None)
+    listeningPorts: str = field(default=None)
+    env: str = field(default=None)
+    privileged: str = field(default=None)
+    capabilities_added: str = field(default=None)
+    capabilities_dropped: str = field(default=None)
+    allowPrivilegeEscalation: str = field(default=None)
+    runAsUser: str = field(default=None)
+    runAsNonRoot: str = field(default=None)
+    command: str = field(default=None)
+    args: str = field(default=None)
 
 @dataclass
 class PodMetadata:
-    name: str = field(metadata={'key': 'hitchhiker.k8s.pod.name'})
-    namespace: str = field(metadata={'key': 'hitchhiker.k8s.pod.namespace'})
-    uid: str = field(metadata={'key': 'hitchhiker.k8s.pod.uid'})
-    username: str = field(metadata={'key': 'hitchhiker.k8s.pod.user.name'})
-    groups: str = field(metadata={'key': 'hitchhiker.k8s.pod.group.name'})
-    dnsPolicy: str = field(metadata={'key': 'hitchhiker.k8s.pod.dnspolicy'})
-    dnsConfig: str = field(default=None, metadata={'key': 'hitchhiker.k8s.pod.dnsconfig'})
-    serviceAccount: str = field(default=None, metadata={'key': 'hitchhiker.k8s.pod.serviceaccount'})
-    fieldManager: str = field(default=None, metadata={'key': 'hitchhiker.k8s.pod.fieldmanager'})
-    containers: list = field(default_factory=list, metadata={'key': 'hitchhiker.k8s.pod.containers'})
-    hostPid: str = field(default=None, metadata={'key': 'hitchhiker.k8s.pod.host.pid'})
-    hostNetwork: str = field(default=None, metadata={'key': 'hitchhiker.k8s.pod.host.network'})
-    hostIpc: str = field(default=None, metadata={'key': 'hitchhiker.k8s.pod.host.ipc'})
-    hostPathMounts: str = field(default=None, metadata={'key': 'hitchhiker.k8s.pod.host.path.mounts'})
-    hostPathsSensitive: str = field(default=None, metadata={'key': 'hitchhiker.k8s.pod.host.path.sensitive'})
-    hostPathsExist: str = field(default=None, metadata={'key': 'hitchhiker.k8s.pod.host.path.exist'})
-    ownerKind: str = field(default=None, metadata={'key': 'hitchhiker.k8s.pod.owner.kind'})
-    ownerName: str = field(default=None, metadata={'key': 'hitchhiker.k8s.pod.owner.name'})
+    name: str = field(default=None)
+    namespace: str = field(default=None)
+    uid: str = field(default=None)
+    username: str = field(default=None)
+    groups: str = field(default=None)
+    dnsPolicy: str = field(default=None)
+    dnsConfig: str = field(default=None)
+    serviceAccount: str = field(default=None)
+    fieldManager: str = field(default=None)
+    containers: list = field(default_factory=list)
+    hostPid: str = field(default=None)
+    hostNetwork: str = field(default=None)
+    hostIpc: str = field(default=None)
+    hostPathMounts: str = field(default=None)
+    hostPathsSensitive: str = field(default=None)
+    hostPathsExist: str = field(default=None)
+    ownerKind: str = field(default=None)
+    ownerName: str = field(default=None)
 
 def to_dict(obj):
     result = {}
     for f in fields(obj):
-        key = f.metadata.get('key', f.name)
         value = getattr(obj, f.name)
         if isinstance(value, list):
-            result[key] = [to_dict(i) for i in value]
+            result[f.name] = [{"container": to_dict(i)} for i in value]
+        elif hasattr(value, '__dataclass_fields__'):
+            result[f.name] = to_dict(value)
         else:
-            result[key] = value
+            result[f.name] = value
     return result
